520fed751ecaa51971374900832f9990c47a33bc
amavvb <amagdolorito@gmail.com>
2024-07-09 03:07:53 +0800

    student.js

--- student.js
+++ student.js
@@ -72,6 +72,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const logoutButton = document.getElementById("logout-button");
     logoutButton.addEventListener("click", function() {
-        window.location.href = "login.html";
+        window.location.href = "index.html";
     });
 });
